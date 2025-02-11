c07b133039a701cc37a764d5fcdef39880f3d186
Juan Antonio Pineda Amador <juanantpiama@gmail.com>
2025-02-11 12:57:00 +0100

    ok

--- build/script.sh
+++ build/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /usr/share/app/
-export DJANGO_SUPERUSER_PASSWORD=${DJ_PASSWORD} 
-python manage.py createsuperuser --username=${DJ_USER} --noinput
+export DJANGO_SUPERUSER_PASSWORD=$DJ_PASSWORD
+python manage.py createsuperuser --username=$DJ_USER --noinput
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:3000
